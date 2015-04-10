e55bc2c808c0fd14c78d3a9925a07c3a3eb96117
Deepak <deepakrawat2287@gmail.com>
2015-04-10 16:20:14 -0700

    image

--- MediaQueries/src/edu/usc/csci576/mediaqueries/ui/MainFrameUI.java
+++ MediaQueries/src/edu/usc/csci576/mediaqueries/ui/MainFrameUI.java
@@ -2,10 +2,15 @@ package edu.usc.csci576.mediaqueries.ui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.image.BufferedImage;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import edu.usc.csci576.mediaqueries.controller.*;
 
 public class MainFrameUI extends JFrame {
 
@@ -36,11 +41,18 @@ public class MainFrameUI extends JFrame {
 	 */
 	public MainFrameUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1000, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		byte[] bytes = ImageHandler.readImageFromFile("Image1.rgb");
+		BufferedImage originalImg = ImageHandler.toBufferedImage(bytes, 352,
+				288, BufferedImage.TYPE_INT_RGB);
+		JLabel imageBox = new JLabel(new ImageIcon(originalImg));
+		imageBox.setBounds(200, 200, 352, 288);
+		contentPane.add(imageBox);
 	}
 
 }
