b4a236ff4417a001e551fc7ed8fe9d4bfd3e75dc
Aditya Dhulipala <adhulipa@usc.edu>
2015-04-30 05:08:38 -0700

    Imptd compareHist with CORREL comparator.
    This helps perfectly match RGB hists.
    Value ranges 0.0 - 1.0; 0 indicates no macth 1.0 indicates perfect
    match.
    
    Additionaly all the intermediate matches give us a good percentage score
    for near-matching frames.

--- MediaQueries/src/edu/usc/csci576/mediaqueries/controller/CompareHistogram.java
+++ MediaQueries/src/edu/usc/csci576/mediaqueries/controller/CompareHistogram.java
@@ -1,10 +1,13 @@
 package edu.usc.csci576.mediaqueries.controller;
 
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 import java.util.List;
 
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
 import edu.usc.csci576.mediaqueries.model.RGBHistogram;
@@ -20,10 +23,14 @@ public class CompareHistogram {
 	 * based on our heuristic this function will return true.
 	 * 
 	 */
-	public static boolean compareRGBHistogram(List<Mat> hist1, List<Mat> hist2) {
+	public static double[] compareRGBHistogram(List<Mat> hist1, 
+			List<Mat> hist2, int CV_COMP_TYPE) {
+		
+		
 		Mat b1 = new Mat(288, 352, CvType.CV_32F);
 		Mat g1 = new Mat(288, 352, CvType.CV_32F);
 		Mat r1 = new Mat(288, 352, CvType.CV_32F);
+		
 		Mat b2 = new Mat(288, 352, CvType.CV_32F);
 		Mat g2 = new Mat(288, 352, CvType.CV_32F);
 		Mat r2 = new Mat(288, 352, CvType.CV_32F);
@@ -31,34 +38,47 @@ public class CompareHistogram {
 		hist1.get(0).convertTo(b1, CvType.CV_32F);
 		hist1.get(1).convertTo(g1, CvType.CV_32F);
 		hist1.get(2).convertTo(r1, CvType.CV_32F);
+		
 		hist2.get(0).convertTo(b2, CvType.CV_32F);
 		hist2.get(1).convertTo(g2, CvType.CV_32F);
 		hist2.get(2).convertTo(r2, CvType.CV_32F);
-		double b_ret = Imgproc.compareHist(b1, b2, Imgproc.CV_COMP_BHATTACHARYYA);
-		double g_ret = Imgproc.compareHist(g1, g2, Imgproc.CV_COMP_BHATTACHARYYA);
-		double r_ret = Imgproc.compareHist(r1, r2, Imgproc.CV_COMP_BHATTACHARYYA);
+		
+		double b_ret = Imgproc.compareHist(b1, b2, CV_COMP_TYPE);
+		double g_ret = Imgproc.compareHist(g1, g2, CV_COMP_TYPE);
+		double r_ret = Imgproc.compareHist(r1, r2, CV_COMP_TYPE);
+		
 		
 		System.out.println(r_ret);
 		System.out.println(g_ret);
 		System.out.println(b_ret);
 		
-		return false;
+		return new double[]{r_ret, g_ret, b_ret};
 	}
 	
 	public static void main(String[] args) {
-		List<Mat> rgbmats1 = RGBHistogram.getRGBMat("database/starcraft/StarCraft020.rgb", 352, 288);
-		List<Mat> rgbmats2 = RGBHistogram.getRGBMat("database/flowers/flowers100.rgb", 352, 288);
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
+		String q = "database/starcraft/StarCraft035.rgb";
+		String d = "database/flowers/flowers001.rgb";				
+		//d = "database/starcraft/StarCraft039.rgb";
+		
+		List<Mat> rgbmats1 = RGBHistogram.getRGBMat(q, 352, 288);
+		List<Mat> rgbmats2 = RGBHistogram.getRGBMat(d, 352, 288);
 		
 		//List<Mat> rgbmats1 = RGBHistogram.getRGBMat("database/flowers/flowers001.rgb", 352, 288);
 		//List<Mat> rgbmats2 = RGBHistogram.getRGBMat("database/flowers/flowers001.rgb", 352, 288);
 		
-		compareRGBHistogram(rgbmats1, rgbmats2);
 		
-		byte[] bytes = ImageHandler.readImageFromFile("database/starcraft/StarCraft020.rgb");
+		double[] diffs = compareRGBHistogram(rgbmats1, rgbmats2, 
+				Imgproc.CV_COMP_CORREL);
+		
+		System.out.println(Arrays.toString(diffs));
+		
+		byte[] bytes = ImageHandler.readImageFromFile(q);
 		BufferedImage img1 = ImageHandler.toBufferedImage(bytes, 352,
 				288, BufferedImage.TYPE_INT_RGB);
 		
-		byte[] bytes1 = ImageHandler.readImageFromFile("database/flowers/flowers100.rgb");
+		byte[] bytes1 = ImageHandler.readImageFromFile(d);
 		BufferedImage img2 = ImageHandler.toBufferedImage(bytes1, 352,
 				288, BufferedImage.TYPE_INT_RGB);
 		
