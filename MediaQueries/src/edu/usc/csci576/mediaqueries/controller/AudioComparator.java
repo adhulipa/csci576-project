7c585a0eaf0bee2954e687db1410f5cee1f23a60
abhinand_kr <kadavige@usc.edu>
2015-05-03 00:41:43 -0700

    AudioComparatorResult; Cherry picked commit 44243ae962058747a83a354caa56a8801c6c2b19

--- MediaQueries/src/edu/usc/csci576/mediaqueries/controller/AudioComparator.java
+++ MediaQueries/src/edu/usc/csci576/mediaqueries/controller/AudioComparator.java
@@ -1,6 +1,8 @@
 package edu.usc.csci576.mediaqueries.controller;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -12,32 +14,32 @@ import org.opencv.core.Core;
 import com.musicg.fingerprint.FingerprintSimilarity;
 import com.musicg.wave.Wave;
 
+import edu.usc.csci576.mediaqueries.model.AudioComparatorResult;
 import edu.usc.csci576.mediaqueries.model.AudioData;
 import edu.usc.csci576.mediaqueries.parallel.AudioChecker;
 
 public class AudioComparator
 {
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args)
+
+	private static ArrayList<AudioComparatorResult> getSimilarAudios(
+			String queryWavPath)
 	{
 		ExecutorService audioCheckExecutor = Executors.newFixedThreadPool(1);
 		
 		String[] dataset = { "starcraft", "flowers", "interview", "movie",
 				"sports", "musicvideo", "traffic" };
-		
-		String queryWavPath = "queries/Not From Searching Content/HQ4/HQ4.wav"; 
 
 		ArrayList<Future<AudioData>> scores = new ArrayList<Future<AudioData>>();
-		for(String item : dataset)
+		for (String item : dataset)
 		{
 			String itemPath = String.format("database/%s/%s.wav", item, item);
 			AudioChecker audioChecker = new AudioChecker(itemPath, queryWavPath);
 			Future<AudioData> score = audioCheckExecutor.submit(audioChecker);
 			scores.add(score);
 		}
-		
+
+		ArrayList<AudioComparatorResult> results = new ArrayList<AudioComparatorResult>();
+
 		for (int i = 0; i < scores.size(); i++)
 		{
 			try
@@ -45,15 +47,34 @@ public class AudioComparator
 				Future<AudioData> audioData = scores.get(i);
 				FingerprintSimilarity fs = audioData.get().getFs();
 				String dataWav = audioData.get().getWavFilePath();
-				
-				System.out.println(fs.getScore() + " " + dataWav);
+
+//				System.out.println(fs.getSimilarity() + " " + dataWav);
+
+				results.add(new AudioComparatorResult(dataWav, fs.getSimilarity(),
+						fs.getScore(), fs.getsetMostSimilarTimePosition(), fs
+								.getMostSimilarFramePosition()));
 			} catch (InterruptedException | ExecutionException e)
 			{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 		audioCheckExecutor.shutdown();
+
+		return results;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		ArrayList<AudioComparatorResult> audioResult = getSimilarAudios("query/Not From Searching Content/HQ4/HQ4.wav");
+		Collections.sort(audioResult, new AudioComparatorResult());
+		for(AudioComparatorResult res : audioResult)
+		{
+			System.out.println(res.getSimilarity() + " " + res.getAudioFileName());
+		}
 	}
 }
